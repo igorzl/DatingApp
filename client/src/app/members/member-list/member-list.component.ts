c61effdbb9c2e9b6189f0533cbd0ebbc243f9e4e
Igor Zlobinski <igor_zlobinski@bezeqint.net>
2021-06-18 15:36:44 +0300

    End of section 9

--- client/src/app/members/member-list/member-list.component.ts
+++ client/src/app/members/member-list/member-list.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Member } from 'src/app/_models/member';
+import { MembersService } from 'src/app/_services/members.service';
 
 @Component({
   selector: 'app-member-list',
@@ -6,10 +8,17 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./member-list.component.css']
 })
 export class MemberListComponent implements OnInit {
+  members!: Member[];
 
-  constructor() { }
+  constructor(private memberService: MembersService) { }
 
   ngOnInit(): void {
+    this.loadMembers();
+  }
+
+  loadMembers() {
+    //don't need any error handling (see error.interceptor.ts)
+    this.memberService.getMembers().subscribe(mems => this.members = mems);
   }
 
 }
